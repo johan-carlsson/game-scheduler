dd49b0dda7c0e9894afd932d06e24b6a7366c653
Johan Carlsson <johancarlsson@me.com>
2018-05-31 21:45:20 +0200

    Added comments and a description the applications data flow

--- src/game_scheduler/core.clj
+++ src/game_scheduler/core.clj
@@ -2,14 +2,41 @@
 (require '[clojure.string :as str])
 (require '[clojure.set :refer [intersection]])
 
-; The general idea is to sort the possible opponents for each group.
-; The group that has the least number of possible opponents gets to select it opponent first.
-; If two or more groups has the same number of possible opponents the first one gets to make the selection (every thing is sorted/ordered)
+; The group that has the least number of possible opponents gets to select it's opponent first.
+; If two or more groups has the same number of possible opponents the first one gets to make the selection (everything is sorted/ordered)
 ; It will always select the first opponent in its list of possible opponents
 ; The solution is recursive and removes available groups and possible opponents as the loop progresses
 ; The structure of the data that runs in the loop looks just the valid-opponents-by-group
 
-; So given ABCD the structur will look like this in the first run (in the loop)
+; So given ABCD as best thirds the possible-maches will look like this in the first run (in the loop)
+; ((\A #{\C \D})
+;  (\B #{\A \C \D}) 
+;  (\C #{\A \B}) 
+;  (\D #{\B}))
+;
+;
+; Since D only can play B, that match is given.
+; Group D and opponent B are removed from the data set and the loop recurs and possible-maches are:
+; ((\A #{\C}) 
+;  (\C #{\A \B}) 
+;  (\D #{\B}))
+; 
+; Now both A and D has one possible opponent.
+; In this case the first group \A selects its only possible opponent \C
+
+; Given ACBE each group will have 2 possible opponents:
+; ((\A #{\C \E})
+;  (\B #{\A \C}) 
+;  (\C #{\A \B}) 
+;  (\D #{\B \E}))
+;
+; In this case the first group \A will select it's first posible opponent \C
+; This process continues until all matches are made 
+
+; The commands to produce the above data structures are:
+; (possible-maches groups (set (seq "ABCD")))
+; (possible-maches (disj groups \B) (set (seq "ABC")))
+
 
 (def groups #{\A \B \C \D})
 
@@ -38,22 +65,28 @@
 (defn match-to-record [match]
   (let [[group opponents] match]
     (->Match group opponents)))
+
 ; (match-to-record (first {\B #{\D \C}}))
+; (apply match-to-record {\B #{\D \C}})
 
 (defn sort-accending-by-number-of-possible-opponents [matches]
   (sort-by #(count (:opponents (match-to-record %))) matches))
+
 ; (sort-accending-by-number-of-possible-opponents {\B #{\D \C} \C #{\A}})
 
 ; Removes groups that has no possible opponents left
 (defn remove-empty-groups [groups]
   (filter #(not (empty? (last %))) groups))
 
+; TODO: Should this be renamed to possible-groups
 (defn possible-maches [groups best-thirds]
   (map
    #(list % (intersection (get valid-opponents-by-group %) best-thirds))
    groups))
+
 ; (possible-maches groups (set (seq "ABCD")))
 
+; TODO: Should i rename match to game next-game
 (defn find-next-match [groups best-thirds]
   (->
    (possible-maches groups best-thirds)
@@ -65,7 +98,9 @@
 (defn create-schedule [groups best-thirds result]
   (let [next-match (match-to-record (find-next-match groups best-thirds))]
     (if (empty? best-thirds)
+      ; Where done and return the result
       (map last (sort-by #(first %) result))
+      ; Where not done, lets continue
       (let [group (:group next-match)
             third (first (:opponents next-match))]
         (create-schedule (disj groups group)
